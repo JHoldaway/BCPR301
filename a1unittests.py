ec6019058652e8a9d5a24c31cb3eab89f60d60be
JHoldaway <49139061+JHoldaway@users.noreply.github.com>
2019-05-11 16:46:18 +1200

    Uploaded Final Version Of Assicnment 1

--- a1unittests.py
+++ a1unittests.py
@@ -5,120 +5,120 @@ import controller
 
 class BasicFunctionality(unittest.TestCase):
     def setUp(self):
-        self.myinterpreter = Interpreter()
+        self.my_interpreter = Interpreter()
 
     def test_add_class(self):
-        self.myinterpreter.add_class("testclass", [], [], [])
-        self.assertEqual(self.myinterpreter.all_my_classes[0].name, "testclass")
+        self.my_interpreter.add_class("testclass", [], [], [])
+        self.assertEqual(self.my_interpreter.all_my_classes[0].name, "testclass")
 
 class SpecificFunctions(unittest.TestCase):
     def setUp(self):
-        self.myinterpreter = Interpreter()
-        self.myinterpreter.add_file("example.txt", "testmodule")
-        self.myinterpreter.read_file()
+        self.my_interpreter = Interpreter()
+        self.my_interpreter.add_file("example.txt", "testmodule")
+        self.my_interpreter.read_file()
 
     def test_classes_added(self):
-        self.assertEqual(len(self.myinterpreter.all_my_classes), 4)
+        self.assertEqual(len(self.my_interpreter.all_my_classes), 4)
 
     def test_module_added(self):
-        self.assertEqual(len(self.myinterpreter.all_my_modules), 1)
+        self.assertEqual(len(self.my_interpreter.all_my_modules), 1)
 
     def test_modulename(self):
-        themodule = self.myinterpreter.all_my_modules[0]
-        modulename = themodule.module_name
-        self.assertEqual(modulename, "testmodule")
+        the_module = self.my_interpreter.all_my_modules[0]
+        module_name = the_module.module_name
+        self.assertEqual(module_name, "testmodule")
 
     def test_class_names(self):
-        themodule = self.myinterpreter.all_my_modules[0]
-        classnames = [i.name for i in themodule.all_my_classes]
-        self.assertIn("ClassBuilder", classnames)
-        self.assertIn("Attribute", classnames)
-        self.assertIn("Method", classnames)
-        self.assertIn("Relationship", classnames)
+        the_module = self.my_interpreter.all_my_modules[0]
+        class_names = [i.name for i in the_module.all_my_classes]
+        self.assertIn("ClassBuilder", class_names)
+        self.assertIn("Attribute", class_names)
+        self.assertIn("Method", class_names)
+        self.assertIn("Relationship", class_names)
 
     def test_relationships(self):
-        themodule = self.myinterpreter.all_my_modules[0]
-        theclass = [i for i in themodule.all_my_classes if i.name == "ClassBuilder"]
-        self.assertEqual(len(theclass[0].all_my_composite_classes), 3)
+        the_module = self.my_interpreter.all_my_modules[0]
+        the_class = [i for i in the_module.all_my_classes if i.name == "ClassBuilder"]
+        self.assertEqual(len(the_class[0].all_my_composite_classes), 3)
 
     def test_methods(self):
-        themodule = self.myinterpreter.all_my_modules[0]
-        theclass = [i for i in themodule.all_my_classes if i.name == "ClassBuilder"]
-        self.assertEqual(len(theclass[0].all_my_methods), 4)
+        the_module = self.my_interpreter.all_my_modules[0]
+        the_class = [i for i in the_module.all_my_classes if i.name == "ClassBuilder"]
+        self.assertEqual(len(the_class[0].all_my_methods), 4)
 
     def test_attributes(self):
-        themodule = self.myinterpreter.all_my_modules[0]
-        theclass = [i for i in themodule.all_my_classes if i.name == "ClassBuilder"]
-        self.assertEqual(len(theclass[0].all_my_attributes), 4)
+        the_module = self.my_interpreter.all_my_modules[0]
+        the_class = [i for i in the_module.all_my_classes if i.name == "ClassBuilder"]
+        self.assertEqual(len(the_class[0].all_my_attributes), 4)
 
     def test_methods_2(self):
-        themodule = self.myinterpreter.all_my_modules[0]
+        themodule = self.my_interpreter.all_my_modules[0]
         theclass = [i for i in themodule.all_my_classes if i.name == "Method"]
         self.assertEqual(len(theclass[0].all_my_methods), 2)
 
     def test_attributes_2(self):
-        themodule = self.myinterpreter.all_my_modules[0]
-        theclass = [i for i in themodule.all_my_classes if i.name == "Method"]
-        self.assertEqual(len(theclass[0].all_my_attributes), 3)
+        the_module = self.my_interpreter.all_my_modules[0]
+        the_class = [i for i in the_module.all_my_classes if i.name == "Method"]
+        self.assertEqual(len(the_class[0].all_my_attributes), 3)
 
     def test_makes_files(self):
-        self.myinterpreter.write_modules()
+        self.my_interpreter.write_modules()
         self.assertTrue(os.path.isfile("./testmodule/attribute.py"))
         self.assertTrue(os.path.isfile("./testmodule/classbuilder.py"))
         self.assertTrue(os.path.isfile("./testmodule/method.py"))
         self.assertTrue(os.path.isfile("./testmodule/relationship.py"))
 
     def test_class_representation(self):
-        themodule = self.myinterpreter.all_my_modules[0]
-        theclass = [i for i in themodule.all_my_classes if i.name == "Attribute"]
-        stringrep = theclass[0].print_class()
+        the_module = self.my_interpreter.all_my_modules[0]
+        the_class = [i for i in the_module.all_my_classes if i.name == "Attribute"]
+        stringrep = the_class[0].print_class()
         self.assertRegex(stringrep, "class Attribute:")
 
     def test_classes_get_methods(self):
-        themodule = self.myinterpreter.all_my_modules[0]
-        theclass = [i for i in themodule.all_my_classes if i.name == "Attribute"]
-        stringrep = theclass[0].print_class()
+        the_module = self.my_interpreter.all_my_modules[0]
+        the_class = [i for i in the_module.all_my_classes if i.name == "Attribute"]
+        stringrep = the_class[0].print_class()
         self.assertRegex(stringrep, "def __init__\(self\):")
         self.assertRegex(stringrep, "def find_type\(self, new_type\)")
         self.assertRegex(stringrep, "def __str__\(self\)")
 
     def test_classes_get_attributes(self):
-        themodule = self.myinterpreter.all_my_modules[0]
-        theclass = [i for i in themodule.all_my_classes if i.name == "Attribute"]
-        stringrep = theclass[0].print_class()
+        the_module = self.my_interpreter.all_my_modules[0]
+        the_class = [i for i in the_module.all_my_classes if i.name == "Attribute"]
+        stringrep = the_class[0].print_class()
         self.assertRegex(stringrep, "self.name")
         self.assertRegex(stringrep, "self.type")
 
     def test_classes_get_relationships(self):
-        themodule = self.myinterpreter.all_my_modules[0]
-        theclass = [i for i in themodule.all_my_classes if i.name == "ClassBuilder"]
-        stringrep = theclass[0].print_class()
+        the_module = self.my_interpreter.all_my_modules[0]
+        the_class = [i for i in the_module.all_my_classes if i.name == "ClassBuilder"]
+        stringrep = the_class[0].print_class()
         self.assertRegex(stringrep, "self.all_my_Attributes = \[\]")
         self.assertRegex(stringrep, "self.all_my_Methods = \[\]")
         self.assertRegex(stringrep, "self.all_my_Relationships = \[\]")
 
 class ControllerTests(unittest.TestCase):
     def setUp(self):
-        self.mycontroller = controller.Controller()
+        self.my_controller = controller.Controller()
 
     def test_controller_runs(self):
         try:
-            self.mycontroller.go()
+            self.my_controller.go()
         except:
             self.fail("The controller raised an exception")
 
     def test_controller_saves(self):
-        self.mycontroller.go()
-        self.mycontroller.save_state("save.p")
+        self.my_controller.go()
+        self.my_controller.save_state("save.p")
         self.assertTrue(os.path.isfile("./save.p"))
     
     def test_controller_loads(self):
-        self.mycontroller.go()
-        self.mycontroller.save_state("save.p")
-        self.mycontroller = controller.Controller()
-        self.mycontroller.load_state("save.p")
-        theinterpreter = self.mycontroller.myinterpreter
-        self.assertEqual(len(theinterpreter.all_my_classes), 4)
+        self.my_controller.go()
+        self.my_controller.save_state("save.p")
+        self.my_controller = controller.Controller()
+        self.my_controller.load_state("save.p")
+        the_interpreter = self.my_controller.my_interpreter
+        self.assertEqual(len(the_interpreter.all_my_classes), 4)
 
 
 if __name__ == '__main__':
--- controller.py
+++ controller.py
@@ -1,25 +1,86 @@
 from module_builder.interpreter import Interpreter
 import pickle
+import sys
+import tkinter as tk
+from tkinter import filedialog
+from cmd import *
 
 
-class Controller:
+class Controller(Cmd):
+    """Controller class for python module builder."""
 
-    def __init__(self):
-        self.myinterpreter = Interpreter()
+    prompt = ">>>"
+    source_file = None
+    module_directory = None
+
+    # def __init__(self):
+    #    """Initialises the controller."""
+    #    self.my_interpreter = Interpreter()
+    #    self.the_window = None
+
+    def _open_files(self):
+        """Method for opening files from GUI"""
+        input_file = filedialog.askopenfilename()
+        output_directory = filedialog.askdirectory()
+        self.my_interpreter.add_file(input_file, output_directory)
+        self.my_interpreter.read_file()
+        self.my_interpreter.write_modules()
 
     def go(self):
-        self.myinterpreter.add_file("example.txt", "testmodule")
-        self.myinterpreter.read_file()
-        self.myinterpreter.write_modules()
+        """Defines the usual behaviour of the controller"""
+        if len(sys.argv) == 1:
+            self.my_interpreter.add_file("example.txt", "testmodule")
+            self.my_interpreter.read_file()
+            self.my_interpreter.write_modules()
+        elif len(sys.argv) == 2 and "-?" in sys.argv or "/?" in sys.argv:
+            sys.stdout.write("Converts pluntuml file to python module")
+            sys.stdout.write("\n\ncontroller.py [input_file [output_location]]")
+            sys.stdout.write("\n\nIf no parameters, the program runs an example version")
+            sys.stdout.write("\ninput_file: the path of the file to use as input, defaults to example.txt")
+            sys.stdout.write("\noutput_location: the location of the directory to output the module")
+            sys.stdout.write("\n(defaults to testmodule)\n\n")
+        elif len(sys.argv) == 2:
+            try:
+                filename = sys.argv[1]
+                self.my_interpreter.add_file(filename, "testmodule")
+                self.my_interpreter.read_file()
+                self.my_interpreter.write_modules()
+                # sys.stdout.write("success")
+            except FileNotFoundError:
+                sys.stdout.write("Error loading file, file does not exist.")
+        elif len(sys.argv) == 3:
+            try:
+                file_to_read = sys.argv[1]
+                module_name = sys.argv[2]
+                self.my_interpreter.add_file(file_to_read, module_name)
+                self.my_interpreter.read_file()
+                self.my_interpreter.write_modules()
+            except FileNotFoundError:
+                sys.stdout.write("Error loading file or writing module, please check both inputs.")
+        else:
+            sys.stdout.write("Too many arguments")
+
+    def go_tk(self):
+        """Graphical version of the go method"""
+        self.the_window = tk.Tk()
+        menubar = tk.Menu(self.the_window)
+        file_menu = tk.Menu(menubar, tearoff=0)
+        file_menu.add_command(label="Open PlantUML File", command=self._open_files)
+        file_menu.add_command(label="Exit", command=self.the_window.quit)
+        menubar.add_cascade(label="File", menu=file_menu)
+        self.the_window.config(menu=menubar)
+        self.the_window.mainloop()
+
 
     # def goload(self):
     #   self.myinterpreter.write_modules()
 
     def save_state(self, filename):
+        """Uses pickle to save the state of the interpreter to filename"""
         try:
-            thefile = open(filename, "wb")
-            pickle.dump(self.myinterpreter, thefile)
-            thefile.close()
+            the_file = open(filename, "wb")
+            pickle.dump(self.my_interpreter, the_file)
+            the_file.close()
         except pickle.PicklingError:
             print("Error with saving: Unpickleable object passed to pickler.")
         except FileNotFoundError:
@@ -28,10 +89,11 @@ class Controller:
             print("Unexpected error while saving")
 
     def load_state(self, filename):
+        """Uses pickle to load saved state of the interpreter from filename"""
         try:
-            thefile = open(filename, "rb")
-            self.myinterpreter = pickle.load(thefile)
-            thefile.close()
+            the_file = open(filename, "rb")
+            self.my_interpreter = pickle.load(the_file)
+            the_file.close()
         except pickle.UnpicklingError:
             print("Error loading file: Issue with file format")
         except AttributeError:
@@ -47,11 +109,63 @@ class Controller:
         except:
             print("Unexpected error while loading")
 
+    def do_source(self, args):
+        self.source_file = args
+        print("Source file set.")
+
+    def do_directory(self, args):
+        self.module_directory = args
+        print("Target directory set.")
+
+    def do_interpret(self, args):
+        if self.source_file is None:
+            print("Sorry, please choose a source file.")
+        elif self.module_directory is None:
+            print("Sorry, please choose a directory.")
+        else:
+            the_interpreter = Interpreter()
+            the_interpreter.add_file(self.source_file, self.module_directory)
+            the_interpreter.write_modules()
+            print("Files Interpreted")
+
+    def do_quit(self, args):
+        return True
+
+    def do_EOF(self, args):
+        return True
+
+    def help_source(self):
+        print("""---
+        Set the Source File
+        Format: source [file]
+        ---
+        """)
+
+    def help_directory(self):
+        print("""---
+        Set the module directory
+        Format: directory [directory]
+        ---
+        """)
+
+    def help_interpret(self):
+        print("""---
+        Interpret the files.
+        Outputs to the directory set in the directory command.
+        Processes the file set in the source command.
+        Format: interpret
+        ---
+        """)
+
 
 def main():
-    mycontroller = Controller()
-    mycontroller.go()
-    mycontroller.save_state("save.p")
+    my_controller = Controller()
+    # my_controller.go_tk()
+    # my_controller.go()
+    # my_controller.save_state("save.p")
+
+    my_controller.cmdloop()
+
     # mycontroller.load_state("save.p")
     # mycontroller.goload()
 
